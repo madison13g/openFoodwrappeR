51603086c3182d517c8b66034e4562047c12ee64
jonahedmundson <jonahedmundson>
2023-01-20 14:05:43 -0800

    Troubleshooting unit testing.

--- R/calls.R
+++ R/calls.R
@@ -1,1 +1,10 @@
 ### This file interacts with the API to make calls and retrieve data. 
+#test function
+factorial <- function(n){
+  if(n == 0){
+    return(1)
+  } else{
+    return(n * factorial(n - 1))
+  }
+}
+
