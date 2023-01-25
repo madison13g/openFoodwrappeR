dc99ff8dd9176b5f5744d40c602c21df0317f95c
Madison Greenough <70852530+madison13g@users.noreply.github.com>
2023-01-24 16:08:50 -0800

    Update tests_madison.R

--- unittesting/tests_madison.R
+++ unittesting/tests_madison.R
@@ -5,4 +5,9 @@ library('testthat')
 #importing function(s) to test
 
 #test test
-# test_that()
+test_that("quick test check", {
+		expect_equal(13.0, 13)
+		expect_identical(13, 13)
+		expect_equal(1+2,3)
+	}
+)
