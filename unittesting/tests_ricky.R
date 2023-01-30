ff5918e28f4b259d35df343f7898ee706e97f7f7
Frédéric <mf@Rickys-Device.local>
2023-01-30 14:16:12 -0800

    added function testing

--- unittesting/tests_ricky.R
+++ unittesting/tests_ricky.R
@@ -6,7 +6,36 @@
 
 library('testthat')
 
+# importing functions
+try(source('./R/rickys.R'), silent=TRUE)
+# b/c I called jonah's functions with values to make mine work
+# this runs through it
+# is that a fair way to test, I guess so
+# maybe add in product function argument for number if know already
+# defaults to none
 
-test_that("learn how to use",{
-    expect_identical(3,2) # would fail
-})
+
+# prod_name function test
+test_that("Testing prod_name function", {
+  expect_error(prod_name(strawberry))
+  expect_error(prod_name("celebration"))
+  expect_type(prod_name(celebration), 'character')
+}
+)
+
+# sugar_per_100g function test
+test_that("Testing sugar_per_100g function", {
+  expect_error(sugar_per_100g(strawberry))
+  expect_error(sugar_per_100g("celebration"))
+  expect_type(sugar_per_100g(celebration), 'double')
+}
+)
+
+# plot_sugar function test
+test_that("Testing plot_sugar function", {
+  expect_error(plot_sugar(strawberry))
+  expect_error(plot_sugar("celebration"))
+  expect_error(plot_sugar(celebration))
+  expect_type(plot_sugar(list(banani, celebration, chip_ahoy)), 'list')
+}
+)
